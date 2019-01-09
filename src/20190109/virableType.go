d07624bc84f319a28d88adc1883ef60d712a18f1
bny64 <bny64@naver.com>
2019-01-09 22:40:55 +0900

    Today I learned virable type in golang

---
+++ src/20190109/virableType.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* var i int
+	var s string
+	var age int = 10
+	var name string = "Maria"
+	age2 := 10
+	name2 := "Maria"
+
+	fmt.Println(i, s, age, name, age2, name2) */
+
+	/* var x, y int = 30, 50
+	var age, name = 10, "Maria"
+
+	fmt.Println(x, y, age, name)
+
+	a, b, c, d := 1, 3.4, "Hello, world", false
+	fmt.Println(a, b, c, d) */
+
+	var (
+		x, y      int = 30, 50
+		age, name     = 10, "Maria"
+	)
+
+	fmt.Println(x, y, age, name)
+
+	//main2()
+	main3()
+}
