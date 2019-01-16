04a5476438c1ee6dc14f2a29fd9c02ede9916d54
bny64 <bny64@naver.com>
2019-01-16 21:37:34 +0900

    about function in a golang

---
+++ src/20190116/func2.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+//before main1
+func main() {
+	r := sumsum(2, 3, 4, 5, 6)
+	fmt.Println(r)
+
+	f := factorial(10)
+	fmt.Println(f)
+}
+
+func sumsum(n ...int) int {
+	total := 0
+	for _, value := range n {
+		total += value
+	}
+
+	return total
+}
+
+func factorial(n uint64) uint64 {
+	if n == 0 {
+		return 1
+	}
+
+	return n * factorial(n-1)
+}
