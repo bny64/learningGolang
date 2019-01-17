2a89ce150f8e1523c3c1fa8b5acd408cf6b53fcf
namyulbae <bny64@naver.com>
2019-01-17 09:16:49 +0900

    panic and recover

---
+++ src/20190117/recover.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func f2() {
+	func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	a := [...]int{1, 2}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(a[i])
+	}
+
+}
+
+func main() {
+	f2()
+	fmt.Println("hello.world!")
+}
