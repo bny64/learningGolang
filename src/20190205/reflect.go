b0adfd421df935306f51b81919f5b04e557f4fb3
bny64 <bny64@naver.com>
2019-02-05 14:15:33 +0900

    reflect

---
+++ src/20190205/reflect.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Data struct {
+	a, b int
+}
+
+func main() {
+	num := 1
+	fmt.Println(reflect.TypeOf(num))
+
+	s := "Hello, world!"
+	fmt.Println(reflect.TypeOf(s))
+
+	f := 1.3
+	fmt.Println(reflect.TypeOf(f))
+
+	var data = Data{1, 2}
+	fmt.Println(reflect.TypeOf(data))
+
+}
