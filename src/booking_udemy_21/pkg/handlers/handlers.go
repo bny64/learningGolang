3c2cd877d87eba9b85347e87fa1528f2bc88eeae
namyulbae <bny64@naver.com>
2026-09-01 07:31:32 +0900

    공부중

--- src/booking_udemy_21/pkg/handlers/handlers.go
+++ src/booking_udemy_21/pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/bny64/go-course/pkg/render"
@@ -8,11 +9,13 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
+	log.Println("Requested URL:", r.URL.Path)
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
+	log.Println("Requested URL:", r.URL.Path)
 	render.RenderTemplate(w, "about.page.tmpl")
 
 }
